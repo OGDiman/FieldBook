77dc538e4ea80f3751113885f3d713f9a52dbd8c
OGDiman <fallenmephist@gmail.com>
2017-04-27 17:50:50 +0300

    Создал интерфейс репозитория-хранилища полей плюс фейковую реализацию этого репозитория. Также подключил IoC-контейнер Ninject.

---
+++ FieldBook/Abstract/IFieldsRepository.cs
@@ -0,0 +1,14 @@
+﻿using FieldBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldBook.Abstract
+{
+  interface IFieldsRepository
+  {
+    IEnumerable<OrderDetailsRefItem> Fields { get; }
+  }
+}
---
+++ FieldBook/Concrete/FieldsFakeRepository.cs
@@ -0,0 +1,33 @@
+﻿using FieldBook.Abstract;
+using FieldBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FieldBook.Concrete
+{
+  public class FieldsFakeRepository : IFieldsRepository
+  {
+    private List<OrderDetailsRefItem> fields = new List<OrderDetailsRefItem> {
+        new OrderDetailsRefItem{ 
+          Actual=true, 
+          Archival=false, 
+          Author="Тест автор", 
+          CreateDate=new DateTime(), 
+          Description="Тест description", 
+          DetailName="testDetail", 
+          DetailType="char", 
+          Display="Тест Детейл", 
+          Id=1, 
+          InterfaceType="operator", 
+          Values=null}
+      };
+
+
+    public IEnumerable<OrderDetailsRefItem> Fields
+    {
+      get { return fields; }
+    }
+  }
+}
